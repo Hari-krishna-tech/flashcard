3458321d5d5075115d7c04f06c0590c1f96dbdae
hari-krishna-tech <jvenkadam@gmail.com>
2023-07-24 20:23:43 +0530

    working on showing the deck on landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
+type TDeck = {
+  title: string,
+  _id: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState<string>('')
+  const [decks, setDecks] = useState<TDeck[]>([])
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    fetch('http://localhost:5001/decks', {
+      method: 'POST',
+      body: JSON.stringify({ title }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    },
+    
+    )
+    setTitle('')
+  }
+  useEffect(() => {
+    fetch('http://localhost:5001/decks',{method: 'GET', headers: {'content-type': 'application/json'}})
+      .then(res => res.json())
+      .then(data => setDecks(data))
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <ul className="decks">
+          {decks.map((deck, i) => {
+            return (
+              <li key={deck._id} className="deck">
+                <h2>{deck.title}</h2>
+              </li>
+            )
+          }
+          )}
+
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='deck-title'>Deck Title</label>
+
+          <input type="text" id='deck-title' value={title}
+            onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(e.target.value)
+            }}/>
+
+            <button type='submit'>Create Deck</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import {config} from "dotenv";
 config();
 
@@ -9,11 +10,18 @@ const app = express();
 const PORT = 5001;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get('/',(req:Request ,res:Response) => {
 
     res.send("hello world");
 })
+
+app.get("/decks", async (req: Request, res: Response) => {
+    const decks = await Deck.find();
+    res.json(decks);
+    });
 
 
 app.post("/decks", async (req: Request, res: Response) => {
