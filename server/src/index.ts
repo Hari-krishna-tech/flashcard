ed45a5eafeb18798bbdd6e99660d7b1957a86dac
hari-krishna-tech <jvenkadam@gmail.com>
2023-07-24 19:23:17 +0530

    created /decks endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,31 @@
 import express, {Request, Response} from "express";
 import mongoose from "mongoose";
 
+
+import Deck from "./models/Deck";
+
 const app = express();
+const PORT = 5001;
 
-//mongodb+srv://hari:hari@authentification-demo.7swcq.mongodb.net/?retryWrites=true&w=majority
-const db = mongoose.connect("mongodb+srv://hari:hari@authentification-demo.7swcq.mongodb.net/?retryWrites=true&w=majority");
-
-
+app.use(express.json());
 
 app.get('/',(req:Request ,res:Response) => {
 
     res.send("hello world");
 })
 
-app.listen(5001);
+
+app.post("/decks", async (req: Request, res: Response) => {
+    console.log(req.body);
+    const deck = new Deck({
+        title: req.body.title || "Untitled Deck",
+        });
+    const createdDeck = await deck.save();
+    res.json(createdDeck);
+    });
+mongoose.connect("mongodb+srv://hari:hari1234@authentification-demo.7swcq.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+    console.log("connected to DB and listening on port " + PORT);
+app.listen(PORT);
+
+})
