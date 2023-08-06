1ee0bdfba92e697987d085c3f0b96424fe9088fb
hari-krishna-tech <jvenkadam@gmail.com>
2023-08-06 18:12:16 +0530

    api_url changed in frontend

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,2 +1,3 @@
 // export const API_URL = 'http://localhost:5001';
-export const API_URL = "https://carnelian-fantasy-foe.glitch.me"
+// export const API_URL = "https://carnelian-fantasy-foe.glitch.me"
+export const API_URL = "https://flashcard-api-five.vercel.app"
